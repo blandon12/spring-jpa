053cd5eaec74024dee00775789f2b8168927b126
Chunxin Xu <chunxin.xu@worldfirst.com>
2018-03-11 14:20:30 +0000

    initial commit

---
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
@@ -0,0 +1,56 @@
+package guru.springframework.spring5webapp.bootstrap;
+
+import guru.springframework.spring5webapp.model.Author;
+import guru.springframework.spring5webapp.model.Book;
+import guru.springframework.spring5webapp.model.Publisher;
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
+    private AuthorRepository authorRepository;
+    private BookRepository bookRepository;
+    private PublisherRepository publisherRepository;
+
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        initData();
+    }
+
+    private void initData() {
+
+        Author eric = new Author("Eric", "Evans");
+        Publisher aaa = new Publisher("publisher one", "address one");
+
+        publisherRepository.save(aaa);
+
+
+        Book ddd = new Book("book one", "1234", aaa);
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+
+        Author rod = new Author("Rod", "Johnson");
+        Publisher bbb = new Publisher("publisher two", "address two");
+
+        publisherRepository.save(bbb);
+
+        Book noEJB = new Book("book two", "2345", bbb);
+        rod.getBooks().add(noEJB);
+
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+    }
+}
